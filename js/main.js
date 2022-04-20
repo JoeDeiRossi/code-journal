ca08fbace3317d193ce2a927d66ca545579fbf74
Joe Dei Rossi <deirossijoseph@gmail.com>
2022-04-19 20:49:53 -0600

    Listen for 'submit' events on the journal entry form and 6 substeps involving updating the data model and resetting the form. #1

--- js/main.js
+++ js/main.js
@@ -12,3 +12,24 @@ urlInput.addEventListener('input', function (event) {
     imagePreview.src = 'images/placeholder-image-square.jpg';
   }
 });
+
+const form = document.querySelector('form');
+const titleInput = document.querySelector('#title');
+const notesInput = document.querySelector('textarea');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const entryObject = {};
+  entryObject.title = titleInput.value;
+  entryObject.url = urlInput.value;
+  entryObject.notes = notesInput.value;
+
+  entryObject.entryID = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(entryObject);
+
+  imagePreview.src = 'images/placeholder-image-square.jpg';
+  titleInput.value = '';
+  urlInput.value = '';
+  notesInput.value = '';
+});
